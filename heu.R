88365dca2d94f2f8ded1b7a171d9fee9976d49db
ywlaw <ywlaw>
2008-09-17 15:45:31 +0000

    Relabelled the heuristic algos. Got rid of fcast_errs and fin_errs. Added another heur. Replaced K-L div with RMSE.

--- heu.R
+++ heu.R
@@ -1,7 +1,20 @@
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Simulation of various heuristics
 # To run this simulation, do e.g.:
-# which_data=1; which_heur=0; source('heu.R')
+# which_heur='0'; which_data=1; source('heu.R')
+# Additionally, max_p and max_q can be set externally
+#
+# For convenience, copy and paste this to run in batch mode:
+# which_heur='0'; for(which_data in 1:8) source('heu.R') *
+# which_heur='0a'; for(which_data in 1:8) source('heu.R') *
+# which_heur='1'; max_p=1; max_q=0; for(which_data in 1:8) source('heu.R') *
+# which_heur='1'; max_p=0; max_q=1; for(which_data in 1:8) source('heu.R') *
+# which_heur='1'; max_p=3; max_q=0; for(which_data in 1:8) source('heu.R') *
+# which_heur='1'; max_p=6; max_q=0; for(which_data in 1:8) source('heu.R')
+# which_heur='1'; max_p=max_q=1; for(which_data in 1:8) source('heu.R')
+# which_heur='1'; max_p=max_q=2; for(which_data in 1:8) source('heu.R')
+# which_heur='1'; max_p=max_q=3; for(which_data in 1:8) source('heu.R')
+# which_heur='1'; max_p=max_q=4; for(which_data in 1:8) source('heu.R')
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 require('stats')
 require('flexmix')
@@ -41,13 +54,15 @@ max_idx <- 5000
 max_dsp_smpls <- 500
 
 # select heuristics
-#which_heur <- 1	# to be specified via cmd line
+#which_heur <- '0'	# to be specified via cmd line
 if(!exists("which_heur"))
 	stop('Must specify which_heur')
 
 # maximum p and q
-max_p <- 4
-max_q <- 4
+if(!exists("max_p"))
+	max_p <- 4
+if(!exists("max_q"))
+	max_q <- 4
 
 # maximum forecasting horizon (or maximum skip samples limit in Supriyo's terminology)
 mssl <- 5
@@ -57,7 +72,7 @@ fcast_lim <- 0.90
 lim_qtile <- qnorm(fcast_lim + (1-fcast_lim)/2)
 
 # initialize R environment
-out_fname <- sprintf("%s-%d-%d-%d-%d-%1.1f", data_fname, max_idx, which_heur, max_p, max_q, err_tol)
+out_fname <- sprintf("%s-%d-%s-%d-%d-%1.1f", data_fname, max_idx, which_heur, max_p, max_q, err_tol)
 out <- file(out_fname, 'w')
 debug <- FALSE
 if(debug) {
@@ -80,20 +95,24 @@ if(debug) {
 # 4	Repeat {skip 1 sample, --SkipSamples} until SkipSamples == 0
 # 5 Go to step 2
 #
-# Heuristic 1 (Supriyo's algorithm that uses forecasted values instead of interpolated values)
+# Heuristic 0a (Supriyo's algorithm that uses forecasted values instead of interpolated values)
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 init0 <- function() { #{{{
 	cssl <<- 0		# CurrentSkipSampleSize
-} # }}}
+	max_p <<- 1
+	max_q <<- 0
+} #}}}
 
-init1 <- function() { #{{{
+init0a <- function() { #{{{
 	cssl <<- 0		# CurrentSkipSampleSize
+	max_p <<- 1
+	max_q <<- 0	
 } # }}}
 
-heur0or1 <- function(selector) { #{{{
+heur0core <- function(selector) { #{{{
 	#- - - - - - - - Step 2 - - - - - - - -
 	# predict 1 sample
-	tryCatch(best_model <<- ar.yw(z, order.max=1),
+	tryCatch(best_model <<- ar.yw(z, order.max=max_p),
 		error=function(e) {
 			if(debug) {
 				writeLines(sprintf("[beg_idx=%d] ", beg_idx), sep="")
@@ -111,7 +130,7 @@ heur0or1 <- function(selector) { #{{{
 	z[length(z)] <<- all_data[end_idx+horiz]
 
 	if(cssl > 0) {		
-		if(selector == 0) {
+		if(selector == '0') {
 			# Heuristics 0: use interpolated values as samples, which is done below
 			#    y1 = y0 + (x1-x0)(y2-y0)/(x2-x0)
 			x0 <- length(z)-horiz; y0 <- z[x0]
@@ -122,18 +141,16 @@ heur0or1 <- function(selector) { #{{{
 				write(sprintf(" x1=%d, y1=%f, x0=%d, y0=%f, x2=%f, y2=%f", x1, z[x1], x0, y0, x2, y2), out)
 			}
 		} else {
-			# Heuristics 1: keep forecasts as samples, which has already been done
+			# Heuristics 0a: keep forecasts as samples, which has already been done
 		}
 
 		fin_data[(end_idx+1):(end_idx+cssl)] <<- z[(length(z)-cssl):(length(z)-1)]
-		fcast_errs <- abs(
-			all_data[(end_idx+1):(end_idx+cssl)] - fin_data[(end_idx+1):(end_idx+cssl)])
-		fin_errs <<- c(fin_errs, fcast_errs)
 
 		# diagnostics
 		for(i in 1:cssl) {
+			err <- abs(all_data[end_idx+i] - fin_data[end_idx+i])
 			write(sprintf("*t=%3d, actual=%f, fcast=%f, err=%f", 
-				end_idx+i, all_data[end_idx+i], fin_data[end_idx+i], fcast_errs[i]), out)
+				end_idx+i, all_data[end_idx+i], fin_data[end_idx+i], err), out)
 		}
 	}
 
@@ -150,50 +167,52 @@ heur0or1 <- function(selector) { #{{{
 	}
 
 	#- - - - - - - - Step 4 - - - - - - - -
-	# slide sampling window one step forward
+	# slide sampling window horiz steps forward
 	beg_idx <<- beg_idx + horiz
 	end_idx <<- end_idx + horiz
+	tot_smpls <<- end_idx
 	if(end_idx > max_idx)
 		return(FALSE)	
 	z <<- ts(z[(length(z)-buf_len+1):length(z)], start=beg_idx)
-	tot_smpls <<- end_idx
 	
 	# compare the original samples and our reduced samples
 	#cat("\n all_data[beg_idx:end_idx]=\n   ", all_data[beg_idx:end_idx], file=out)
 	#cat("\n z=", z, file=out)
-	cat("\n diff=", all_data[beg_idx:end_idx]-z, "\n\n", file=out)
+	cat("\n diff=", abs(all_data[beg_idx:end_idx]-z), "\n\n", file=out)
 
 	return(TRUE)
 } #}}}
 
 heur0 <- function() {
-	return(heur0or1(0))
+	return(heur0core('0'))
 }
 
-heur1 <- function() {
-	return(heur0or1(1))
+heur0a <- function() {
+	return(heur0core('0a'))
 }
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# Heuristics 2
+# Heuristics 1
 # 1	Collect 50 samples
-# 2	Predict l samples
-# 3	If k out of l forecasts satisfy criteria, then skip sampling for k
-#	times
+# 2	Forecast l samples
+# 3	If k out of confidence intervals < error threshold, then skip sampling
+#	for k times
 # 4	Slide sampling window k steps forward, i.e. collect k more samples
 # 5 Go back to step 3
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-init2 <- function() { #{{{
+init1 <- function() { #{{{
 } #}}}
 
-heur2 <- function() { #{{{
+heur1 <- function() { #{{{
 	#- - - - - - - - Model identification and estimation - - - - - - - -
 	if(debug) {
 		acf(z, lag.max=50)
 		pacf(z, lag.max=50)
 	}
 
-	min_aic <- 1e+6
+	min_aic <- NULL
+	best_p <- 1	# it might happen that no model can be obtained
+	best_q <- 0	# in which case these are the default p and q
 	for(p in 0:max_p) {
 		for(q in 0:max_q) {
 			if(p == 0 && q == 0) next
@@ -208,7 +227,7 @@ heur2 <- function() { #{{{
 			if(!is.null(model)) {			
 				if(debug) 
 					write(sprintf("[p=%d, q=%d] aic=%f, sigma=%f", p, q, model$aic, sqrt(model$sigma2)), out)
-				if(model$aic < min_aic) {
+				if(length(min_aic) == 0 || model$aic < min_aic) {
 					min_aic <- model$aic
 					best_model <<- model
 					best_p <- p
@@ -228,8 +247,6 @@ heur2 <- function() { #{{{
 	half_intrvl <- lim_qtile*fcast$se			# half of confidence interval (search 'AirPassengers' in R manual)
 	lb <- z_n_l - half_intrvl					# lower bounds of confidence intervals
 	ub <- z_n_l + half_intrvl					# upper bounds of confidence intervals
-	fcast_errs <- abs(
-		all_data[(end_idx+1):(end_idx+mssl)] - z_n_l[1:mssl])
 
 	#- - - - - - - - Skip sampling - - - - - - - -
 	skip <- 0
@@ -239,51 +256,138 @@ heur2 <- function() { #{{{
 			fin_data[end_idx+i] <<- z_n_l[i]
 
 			# diagnostics
+			err <- abs(all_data[end_idx+i] - z_n_l[i])
 			str_in <- ifelse(all_data[end_idx+i] > lb[i] && all_data[end_idx+i] < ub[i],
 				' in', 'nin')
 			write(sprintf("*t=%3d, actual=%f, fcast=%f, err=%f, %s %f-wide [%f, %f]",
-				end_idx+i, all_data[end_idx+i], z_n_l[i], fcast_errs[i],
+				end_idx+i, all_data[end_idx+i], z_n_l[i], err,
 				str_in, ub[i]-lb[i], lb[i], ub[i]), out)
 		} else
 			break
 	}
 	# diagnostics
 	for(i in (skip+1):mssl) {
+		err <- abs(all_data[end_idx+i] - z_n_l[i])
 		str_in <- ifelse(all_data[end_idx+i] > lb[i] && all_data[end_idx+i] < ub[i],
 			' in', 'nin')
 		write(sprintf(" t=%3d, actual=%f, fcast=%f, err=%f, %s %f-wide [%f, %f]",
-			end_idx+i, all_data[end_idx+i], z_n_l[i], fcast_errs[i],
+			end_idx+i, all_data[end_idx+i], z_n_l[i], err,
 			str_in, ub[i]-lb[i], lb[i], ub[i]), out)
 	}
-	if(skip > 0)
-		fin_errs <<- c(fin_errs, fcast_errs[1:skip])
 	tot_skips <<- tot_skips + skip	
 
 	#- - - - - - - - Resume sampling - - - - - - - -
-	# sample slide_step more times before attempting to forecast again
+	# acquire n2smpls samples before attempting to forecast again
 	if(skip > 0)
-		slide_step <- skip
+		n2smpls <- skip
 	else
-		slide_step <- mssl
-	z <<- c(z[1:(buf_len+skip)], all_data[(end_idx+skip+1):(end_idx+skip+slide_step)])
-	beg_idx <<- beg_idx + skip + slide_step
-	end_idx <<- end_idx + skip + slide_step
+		n2smpls <- mssl
+	z <<- c(z[1:(buf_len+skip)], all_data[(end_idx+skip+1):(end_idx+skip+n2smpls)])
+	beg_idx <<- beg_idx + skip + n2smpls
+	end_idx <<- end_idx + skip + n2smpls
+	tot_smpls <<- end_idx
 	if(end_idx > max_idx)
 		return(FALSE)
-	z <<- ts(z[(length(z)-buf_len+1):length(z)], start=beg_idx)
-	tot_smpls <<- end_idx + skip
+	z <<- ts(z[(length(z)-buf_len+1):length(z)], start=beg_idx)	
 
 	# compare the original samples and our reduced samples	
 	#cat("\n all_data[beg_idx:end_idx]=\n       ", all_data[beg_idx:end_idx], file=out)
 	#cat("\n z=", z, "\n\n", file=out)
-	cat("\n diff=", all_data[beg_idx:end_idx]-z, "\n\n", file=out)
+	cat("\n diff=", abs(all_data[beg_idx:end_idx]-z), "\n\n", file=out)
+
+	return(TRUE)
+} #}}}
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Heuristics 2
+# 1	Collect 50 samples
+# 2	Collect and forecast l sample
+# 3	If close, then skip next sample; otherwise go back to Step 2
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+init2 <- function() { #{{{
+} #}}}
+
+heur2 <- function() { #{{{
+	min_aic <- NULL
+	best_p <- 1	# it might happen that no model can be obtained
+	best_q <- 0	# in which case these are the default p and q
+	for(p in 0:max_p) {
+		for(q in 0:max_q) {
+			if(p == 0 && q == 0) next
+			model <- NULL
+			tryCatch(model <- arima(z, order=c(p,d,q), include.mean=TRUE),
+				error=function(e) {
+					if(debug) {
+						writeLines(sprintf("[p=%d, q=%d] ", p, q), sep="")
+						writeLines(toString(e), sep="")
+					}
+				})
+			if(!is.null(model)) {			
+				if(debug) 
+					write(sprintf("[p=%d, q=%d] aic=%f, sigma=%f", p, q, model$aic, sqrt(model$sigma2)), out)
+				if(length(min_aic) == 0 || model$aic < min_aic) {
+					min_aic <- model$aic
+					best_model <<- model
+					best_p <- p
+					best_q <- q
+				}
+			}
+		}
+	}
+	write(sprintf("Best model: p=%d, q=%d, aic=%f, sigma=%f",
+		best_p, best_q, best_model$aic, sqrt(best_model$sigma2)), out)
+
+	#- - - - - - - - Step 2 - - - - - - - -
+	fcast <- predict(best_model, n.ahead=2)		# 1 for comparison, 1 for potential substitute for actual sample
+	z <<- c(z, fcast$pred)						# concatenate predictions to original time series
+	z <<- diffinv(z, differences=d, xi=z_iv)	# recover original time series
+	z_n_l <- z[(length(z)-1):length(z)]			# forecasts
+	z[length(z)-1] <<- all_data[end_idx+1]
+
+	#- - - - - - - - Step 3 - - - - - - - -
+	err <- abs(z[length(z)-1] - z_n_l[1])
+	if(err < err_tol) {
+		# diagnostics
+		write(sprintf("@t=%3d, actual=%f, fcast=%f, err=%f",
+			end_idx+1, z[length(z)-1], z_n_l[1], err), out)
+
+		# update count
+		tot_skips <<- tot_skips + 1	
+
+		# take the forecast as a substitute for the actual sample
+		fin_data[end_idx+2] <<- z_n_l[2]
+
+		# diagnostics
+		err <- abs(all_data[end_idx+2] - fin_data[end_idx+2])
+		write(sprintf("*t=%3d, actual=%f, fcast=%f, err=%f",
+			end_idx+2, all_data[end_idx+2], fin_data[end_idx+2], err), out)
+	} else {	
+		# diagnostics
+		write(sprintf(" t=%3d, actual=%f, fcast=%f, err=%f",
+			end_idx+1, z[length(z)-1], z_n_l[1], err), out)
+
+		# update z with the actual sample
+		z[length(z)] <<- all_data[end_idx+2]
+	}		
+
+	# adjust pointers
+	beg_idx <<- beg_idx + 2
+	end_idx <<- end_idx + 2
+	tot_smpls <<- end_idx
+	if(end_idx > max_idx)
+		return(FALSE)
+	z <<- ts(z[(length(z)-buf_len+1):length(z)], start=beg_idx)
+
+	# compare the original samples and our reduced samples	
+	#cat("\n all_data[beg_idx:end_idx]=\n       ", all_data[beg_idx:end_idx], file=out)
+	#cat("\n z=", z, "\n\n", file=out)
+	cat("\n diff=", abs(all_data[beg_idx:end_idx]-z), "\n\n", file=out)
 
 	return(TRUE)
 } #}}}
 
 #- - - - - - - - Main - - - - - - - -
 fin_data <- all_data
-fin_errs <- NULL
 
 tot_skips <- 0
 tot_smpls <- 0
@@ -294,10 +398,10 @@ best_model <- NULL	# it's good to have this because
 					# the later iteration can use the prev model
 
 # does not return
-func_init <- sprintf("init%d", which_heur)
+func_init <- sprintf("init%s", which_heur)
 
 # returns success (boolean)
-func_heur <- sprintf("heur%d", which_heur)
+func_heur <- sprintf("heur%s", which_heur)
 
 eval(call(func_init))
 
@@ -321,13 +425,16 @@ repeat {
 		break
 }
 
-# compute Kullback-Leibler divergence
-m = cbind(all_data[1:tot_smpls], fin_data[1:tot_smpls])
-kl_div = KLdiv(cbind(all_data[1:tot_smpls], fin_data[1:tot_smpls]))[1,2]
+# compute Kullback-Leibler divergence (wrong)
+#m = cbind(all_data[1:tot_smpls], fin_data[1:tot_smpls])
+#kl_div = KLdiv(cbind(all_data[1:tot_smpls], fin_data[1:tot_smpls]))[1,2]
+
+# computer RMSE
+rmse <- sqrt(mean((all_data[1:tot_smpls]-fin_data[1:tot_smpls])^2))
 
 # display result
-write(sprintf("max_p=%d, max_q=%d, tot_skips=%d(%d), tot_smpls=%d, reductn=%f, kl_div=%g",
-	max_p, max_q, tot_skips, length(fin_errs), tot_smpls, tot_skips/tot_smpls, kl_div), out)
+write(sprintf("max_p=%d, max_q=%d, tot_skips=%d, tot_smpls=%d, reductn=%f, rmse=%g",
+	max_p, max_q, tot_skips, tot_smpls, tot_skips/tot_smpls, rmse), out)
 cat("\n all_data=", all_data[1:tot_smpls], file=out)
 cat("\n fin_data=", fin_data[1:tot_smpls], file=out)
 
@@ -340,18 +447,18 @@ if(debug) {
 # must be done here after because this is a new device
 # note: heavy fine-tuning
 par(mar=c(2.5,2.5,1.5,0.5), mgp=c(1.5,0.5,0), cex.axis=0.8)
-if(tot_smpls <= max_dsp_smpls) {
-	plot(ts(all_data[1:tot_smpls], start=1), main=data_fname, ylab=data_type)
+if(buf_len+max_dsp_smpls <= tot_smpls) {
+	plot(ts(all_data[buf_len:(buf_len+max_dsp_smpls)], start=buf_len), main=data_fname, ylab=data_type)
 } else {
-	plot(ts(all_data[1:max_dsp_smpls], start=1), main=data_fname, ylab=data_type)
+	plot(ts(all_data[1:tot_smpls], start=1), main=data_fname, ylab=data_type)
 }
 par(col='blue')
-if(tot_smpls <= max_dsp_smpls) {
-	lines(ts(fin_data[1:tot_smpls], start=1))
+if(buf_len+max_dsp_smpls <= tot_smpls) {
+	lines(ts(fin_data[buf_len:(buf_len+max_dsp_smpls)], start=buf_len))
 } else {
-	lines(ts(fin_data[1:max_dsp_smpls], start=1))
+	lines(ts(fin_data[1:tot_smpls], start=1))
 }
-legend("bottomright", sprintf('reduction=%f\nkl_div=%g', tot_skips/tot_smpls, kl_div))
+legend("bottomright", sprintf('reduction=%f\nrmse=%g', tot_skips/tot_smpls, rmse))
 par(col='black')
 if(!debug) {
 	dev.off()	# needed to flush the PDF
